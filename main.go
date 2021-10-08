fd6f3e6530d088aa6180d1dff4f4fd748ea28678
Evan Farrer <Evan.Farrer@gmail.com>
2021-10-07 19:50:36 -0600

    Stub out page2pod lambda
    This lambda will be used to convert text/url to a podcast episode hosted
    on AWS.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 201,
+		Body:       "Hello World",
+	}, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
