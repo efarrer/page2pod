ec13f04a65f15adc6715ca4ed4c91c6633edee3c
Evan Farrer <Evan.Farrer@gmail.com>
2021-10-07 20:20:37 -0600

    Deploy the Page2Pod lambda
    Actually deploy it for real.

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import assets = require("@aws-cdk/aws-s3-assets")
+import path = require("path")
 
 export class Page2PodStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create the Page2PodFunction lambda
+    const funcAsset = new assets.Asset(this, 'Page2PodZip', {
+      path: path.join(__dirname, '../../page2pod.zip'),
+    });
+    const handler = new lambda.Function(this, "Page2PodFunction", {
+      runtime: lambda.Runtime.GO_1_X,
+      handler: "page2pod.linux",
+      code: lambda.Code.fromBucket(
+        funcAsset.bucket,
+        funcAsset.s3ObjectKey
+      ),
+      environment: {
+      }
+    });
   }
 }
